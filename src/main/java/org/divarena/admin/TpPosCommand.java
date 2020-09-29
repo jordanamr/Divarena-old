54e476666dc2c20fbf68cc3c1988777bb2defddf
Antideveloppeur <jordan@nexolite.com>
2020-09-29 23:37:51 +0200

    Nique ta race

--- src/main/java/org/divarena/admin/TpPosCommand.java
+++ src/main/java/org/divarena/admin/TpPosCommand.java
@@ -1,6 +1,5 @@
 package org.divarena.admin;
 
-import org.divarena.Divarena;
 import org.divarena.network.ArenaClient;
 import org.divarena.protocol.server.world.EnterInstanceMessage;
 
@@ -14,19 +13,6 @@ public class TpPosCommand extends Command {
     public void execute(ArenaClient client, String[] args) {
         if (args.length < 3) {
             log(client, "Utilisation: tppos [x] [y] [z]");
-            return;
-        }
-        if (args[0].equalsIgnoreCase("start")) {
-            int x = Divarena.getInstance().getConfig().getInt("start.x");
-            int y = Divarena.getInstance().getConfig().getInt("start.y");
-            int z = Divarena.getInstance().getConfig().getInt("start.z");
-            client.getInstance().removeMember(client.getCoach());
-            client.getCoach().setX(x);
-            client.getCoach().setY(y);
-            client.getCoach().setZ(z);
-            client.sendMessage(new EnterInstanceMessage(client.getCoach()));
-            client.getInstance().addMember(client.getCoach());
-            log(client,"Vous vous êtes téléporté aux coordonnées " + x + ", " + y + ", " + z);
             return;
         }
         try {
